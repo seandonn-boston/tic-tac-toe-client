f2baab978e20b9c3f16ec74150da3267c960e28f
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-06-07 14:36:18 -0400

    Additional comments
    This commit features comments at the top of a few files which I know I
    can edit. If I ever accidentally open the wrong index.js file, I will
    know right away. I will add similar comments to the top of most files
    I will be working in. I will delete these comments before submitting
    the final project.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,5 @@
+// HEY SEAN!!! Sean, you may edit this file. Don't edit files that don't have this comment at the top. Delete this comment before deliverables are due
+
 'use strict'
 
 // use require with a reference to bundle the file and use it in this file
